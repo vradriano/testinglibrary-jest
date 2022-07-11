58a3381a22f0efb8d433bf441b59a1a2896c053b
unknown <vradriano@hotmail.com>
2022-07-10 22:20:45 -0300

    Initial commit, setup testing library - jest, add dependecies

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Testing Library</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/tests/setupTests.ts
@@ -0,0 +1,2 @@
+import '@testing-library/jest-dom/extend-expect' // funcionalidades extras para lidar com a DOM.
+
